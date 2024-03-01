ae002d1629b394b91af112e1d242428f3af7de2c
Pinyi Wu <wpypy8@gmail.com>
2024-03-01 17:41:48 +0800

    feat: 🧋 測試功能

--- src/views/front/FrontView.vue
+++ src/views/front/FrontView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="min-vh-100 d-flex flex-column">
-    <VisitorHeader />
+    <div v-if="isLoggedIn">
+      <VisitorHeader />
+    </div>
+    <div v-else>
+      <UserHeader />
+    </div>
     <div class="flex-grow-1">
       <RouterView></RouterView>
     </div>
@@ -10,12 +15,37 @@
 
 <script>
 import VisitorHeader from '@/components/header/VisitorHeader.vue';
+import UserHeader from '@/components/header/UserHeader.vue';
 import UserFooter from '@/components/footer/UserFooter.vue';
 
+const { VITE_URL } = import.meta.env;
+
 export default {
+  data() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  methods: {
+    async checkUser() {
+      try {
+        const token = document.cookie.replace(/(?:(?:^|.*;\s*)leleToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+        this.$http.defaults.headers.common.Authorization = token;
+        await this.$http.get(`${VITE_URL}/dreamboost/user/normal/userprofile`);
+        this.isLoggedIn = true;
+      } catch (error) {
+        this.$router.push({ name: 'member' });
+        // 這邊要加請先登入會員提示
+      }
+    },
+  },
   components: {
     VisitorHeader,
+    UserHeader,
     UserFooter,
+  },
+  mounted() {
+    this.checkUser();
   },
 };
 </script>
