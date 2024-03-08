9695d2572e79dcd84bad0ceebac962f0e6299592
Pinyi Wu <wpypy8@gmail.com>
2024-03-09 00:31:48 +0800

    feat: 🧋 加入驗證功能（待補）

--- src/views/front/DpPayInfo.vue
+++ src/views/front/DpPayInfo.vue
@@ -104,176 +104,218 @@
           <h5 class="mb-3 fw-medium">信用卡付款 <span class="text-danger">*</span></h5>
           <p class="mb-3">指定信用卡可享 3 期 / 零利率、可用銀聯卡、可用國外卡</p>
           <!-- 表單開始 -->
-          <form class="mb-8">
-            <div class="mb-3">
-              <div class="form-check form-check-inline me-11">
-                <input class="form-check-input checked" type="radio" name="payoff" id="payoff">
-                <label class="form-check-label fs-6 text-gray-500" for="payoff">
-                  一次付清
-                </label>
-              </div>
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="installment" id="installment">
-                <label class="form-check-label fs-6 text-gray-500" for="installment">
-                  分 3 期
-                </label>
-              </div>
-            </div>
-            <div class="bg-bgc-paper rounded-3 col-12 mb-6">
-              <p class="fs-6 text-primary-dark px-8 py-4">您了解您的贊助是支持創意專案的一種方式，也了解創意實踐過程中充滿變數，專案不一定能確保回饋。</p>
-            </div>
-            <!-- 加碼和折扣 -->
-            <div class="row d-flex mb-6">
-              <div class="col d-flex flex-column">
-                <label for="sponsors" class="form-label fw-medium">加碼贊助<span class="text-gray-500">（選填）</span></label>
-                <input class="form-control fs-6 border-primary-light" list="datalistOptions" id="sponsors"
-                  placeholder="小小金額是大大的肯定">
-                <datalist id="datalistOptions">
-                  <option value="100"></option>
-                  <option value="200"></option>
-                  <option value="500"></option>
-                </datalist>
-              </div>
-              <div class="col">
-                <label for="discount" class="form-label fw-medium">請輸入折扣碼</label>
-                <input class="form-control fs-6 border-primary-light" id="sponsors">
-              </div>
-            </div>
-            <!-- 收件地點 -->
-            <div class="row mb-6">
-              <div class="col-6">
-                <label for="discount" class="form-label fw-medium">收件地點</label>
-                <select class="form-select border-primary-light fs-6 text-gray-500" aria-label="inTwOrOut">
-                  <option selected value="main-island">台灣（本島）</option>
-                  <option value="outer-lslands">台灣（外島）需付額外運費</option>
-                </select>
-              </div>
-            </div>
-            <div class="row flex-column">
-              <div class="col-6 d-flex mb-4">
-                <div class="col me-2">
-                  <label for="postal-code" class="mb-2 fw-medium">收件地址</label>
-                  <input class="form-control fs-6 border-primary-light" id="sponsors" placeholder="郵遞區號">
+          <Vform @submit="nextStep" ref="form" v-slot="{ errors }">
+            <div class="mb-8">
+              <div class="mb-3">
+                <div class="form-check form-check-inline me-11">
+                  <input class="form-check-input checked" type="radio" name="payoff" id="payoff">
+                  <label class="form-check-label fs-6 text-gray-500" for="payoff">
+                    一次付清
+                  </label>
                 </div>
-                <div class="col mt-2">
-                  <label for="choose-area"></label>
-                  <select class="form-select border-primary-light fs-6 text-gray-500" aria-label="area">
-                    <option selected disabled value="main-island">選擇縣市</option>
-                    <option value="outer-lslands">台北市</option>
+                <div class="form-check form-check-inline">
+                  <input class="form-check-input" type="radio" name="installment" id="installment">
+                  <label class="form-check-label fs-6 text-gray-500" for="installment">
+                    分 3 期
+                  </label>
+                </div>
+              </div>
+              <div class="bg-bgc-paper rounded-3 col-12 mb-6">
+                <p class="fs-6 text-primary-dark px-8 py-4">您了解您的贊助是支持創意專案的一種方式，也了解創意實踐過程中充滿變數，專案不一定能確保回饋。</p>
+              </div>
+              <!-- 加碼和折扣 -->
+              <div class="row d-flex mb-6">
+                <div class="col d-flex flex-column">
+                  <label for="sponsors" class="form-label fw-medium">加碼贊助<span class="text-gray-500">（選填）</span></label>
+                  <input class="form-control fs-6 border-primary-light" list="datalistOptions" id="sponsors"
+                    placeholder="小小金額是大大的肯定">
+                  <datalist id="datalistOptions">
+                    <option value="100"></option>
+                    <option value="200"></option>
+                    <option value="500"></option>
+                  </datalist>
+                </div>
+                <div class="col">
+                  <label for="discount" class="form-label fw-medium">請輸入折扣碼</label>
+                  <input class="form-control fs-6 border-primary-light" id="sponsors">
+                </div>
+              </div>
+              <!-- 收件地點 -->
+              <div class="row mb-6">
+                <div class="col-6">
+                  <label for="discount" class="form-label fw-medium">收件地點</label>
+                  <select class="form-select border-primary-light fs-6 text-gray-500" aria-label="inTwOrOut">
+                    <option selected value="main-island">台灣（本島）</option>
+                    <option value="outer-lslands">台灣（外島）需付額外運費</option>
                   </select>
                 </div>
               </div>
-              <div class="col me-2">
-                <input class="form-control fs-6 border-primary-light" id="sponsors" placeholder="請輸入正確地址以免造成您的權益受損">
-                <label for="postal-code" class="mb-2"></label>
-              </div>
-            </div>
-            <!-- 收件人姓名 -->
-            <div class="col mb-6">
-              <label for="addressee" class="form-label fw-medium position-relative">
-                收件人姓名
-                <span class="text-danger">*
-                  <button type="button" class="btn px-1 py-0 border-0" ref="name" data-bs-toggle="tooltip-name"
-                    data-bs-placement="right" data-bs-custom-class="customTooltip"
-                    data-bs-title="寄送贊助回饋使用，若不正確可能造成投遞失敗。">
-                    <span class="" style="width: 20px;">
-                      <img src="/images/explore/info.svg" class="img-fluid" style="margin-top: -6px;" alt="">
-                    </span>
-                  </button>
-                </span>
-              </label>
-              <input class="form-control fs-6 border-primary-light" id="addressee" placeholder="請輸入真實姓名">
-            </div>
-            <div class="col mb-6">
-              <label for="phone" type="text" class="form-label fw-medium">
-                行動電話
-                <span class="text-danger">*
-                  <button type="button" class="btn px-1 py-0 border-0" ref="phone" data-bs-toggle="tooltip-phone"
-                    data-bs-placement="right" data-bs-custom-class="customTooltip"
-                    data-bs-title="寄送人聯絡及付款確認使用。請填寫全碼，如 0912123456">
-                    <span class="" style="width: 20px;">
-                      <img src="/images/explore/info.svg" class="img-fluid" style="margin-top: -6px;" alt="">
-                    </span>
-                  </button>
-                </span>
-                <div></div>
-              </label>
-              <input class="form-control fs-6 border-primary-light" id="phone" placeholder="請輸入聯絡電話">
-            </div>
-            <!-- 發票資訊-個人 -->
-            <div class="col bg-bgc-paper rounded-3 p-6 mb-6">
-              <h5 class="mb-3 fw-medium">發票資訊 <span class="text-danger">*</span></h5>
-              <div class="mb-3">
-                <div class="form-check form-check-inline me-11">
-                  <input class="form-check-input checked" type="radio" name="personalticket" id="personalticket">
-                  <label class="form-check-label fs-6 text-gray-500" for="personalticket">
-                    個人發票
-                  </label>
+              <div class="row flex-column">
+                <div class="col-6 d-flex mb-4">
+                  <div class="col me-2">
+                    <label for="postal-code" class="mb-2 fw-medium">收件地址</label>
+                    <VField
+                      class="form-control fs-6 border-primary-light"
+                      id="area-num"
+                      name="郵遞區號"
+                      placeholder="郵遞區號"
+                      :class="{ 'is-invalid': errors['郵遞區號'] }"
+                      rules="required"
+                    ></VField>
+                  </div>
+                  <div class="col mt-2">
+                    <label for="choose-area"></label>
+                    <VField
+                      id="area"
+                      name="選擇縣市"
+                      class="form-select border-primary-light fs-6 text-gray-500"
+                      :class="{ 'is-invalid': errors['選擇縣市'] }"
+                      rules="required"
+                      as="select">
+                      <option selected disabled value="">選擇縣市</option>
+                      <option v-for="(city, index) in cities" :key="index" :value="city">{{ city }}</option>
+                    </VField>
+                    <ErrorMessage class="invalid-feedback" name="選擇縣市"></ErrorMessage>
+                  </div>
                 </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="bsticket" id="bsticket">
-                  <label class="form-check-label fs-6 text-gray-500" for="bsticket">
-                    公司發票
-                  </label>
+                <div class="col me-2">
+                  <label for="postal-code" class="mb-2"></label>
+                  <VField
+                    class="form-control fs-6 border-primary-light"
+                    :class="{ 'is-invalid': errors['地址'] }"
+                    name="地址"
+                    id="address"
+                    rules="required"
+                    placeholder="請輸入正確地址以免造成您的權益受損"
+                    v-model="address"
+                  ></VField>
+                  <error-message name="地址" class="invalid-feedback"></error-message>
                 </div>
               </div>
-              <div class="col-6">
-                <label for="phoneticket" class="form-label fs-6">
-                  手機載具
-                  <span class="text-danger">
-                    *
+              <!-- 收件人姓名 -->
+              <div class="col mb-6">
+                <label for="addressee" class="form-label fw-medium position-relative">
+                  收件人姓名
+                  <span class="text-danger">*
+                    <button type="button" class="btn px-1 py-0 border-0" ref="name" data-bs-toggle="tooltip-name"
+                      data-bs-placement="right" data-bs-custom-class="customTooltip"
+                      data-bs-title="寄送贊助回饋使用，若不正確可能造成投遞失敗。">
+                      <span class="" style="width: 20px;">
+                        <img src="/images/explore/info.svg" class="img-fluid" style="margin-top: -6px;" alt="">
+                      </span>
+                    </button>
                   </span>
                 </label>
-                <input class="form-control fs-6 border-primary-light" id="phoneticket" placeholder="例：/AB1CD23">
+                <VField
+                  class="form-control fs-6 border-primary-light"
+                  :class="{ 'is-invalid': errors['姓名'] }"
+                  id="name"
+                  name="姓名"
+                  type="text"
+                  placeholder="請輸入真實姓名"
+                  rules="required"
+                ></VField>
+                <error-message name="姓名" class="invalid-feedback"></error-message>
               </div>
-            </div>
-            <!-- 發票資訊-公司 -->
-            <div class="col bg-bgc-paper rounded-3 p-6 mb-6 d-none">
-              <h5 class="mb-3 fw-medium">發票資訊 <span class="text-danger">*</span></h5>
-              <div class="mb-3">
-                <div class="form-check form-check-inline me-11">
-                  <input class="form-check-input" type="radio" name="personalticket" id="personalticket">
-                  <label class="form-check-label fs-6 text-gray-500" for="personalticket">
-                    個人發票
-                  </label>
-                </div>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input checked" type="radio" name="bsticket" id="bsticket">
-                  <label class="form-check-label fs-6 text-gray-500" for="bsticket">
-                    公司發票
-                  </label>
-                </div>
+              <div class="col mb-6">
+                <label for="phone" type="text" class="form-label fw-medium">
+                  行動電話
+                  <span class="text-danger">*
+                    <button type="button" class="btn px-1 py-0 border-0" ref="phone" data-bs-toggle="tooltip-phone"
+                      data-bs-placement="right" data-bs-custom-class="customTooltip"
+                      data-bs-title="寄送人聯絡及付款確認使用。請填寫全碼，如 0912123456">
+                      <span class="" style="width: 20px;">
+                        <img src="/images/explore/info.svg" class="img-fluid" style="margin-top: -6px;" alt="">
+                      </span>
+                    </button>
+                  </span>
+                </label>
+                <VField
+                  type="tel"
+                  class="form-control fs-6 border-primary-light"
+                  :class="{ 'is-invalid': errors['行動電話'] }"
+                  id="phone"
+                  name="行動電話"
+                  rules="required"
+                  placeholder="請輸入聯絡電話"
+                ></VField>
+                <error-message class="invalid-feedback" name="行動電話"></error-message>
               </div>
-              <div class="row d-flex">
+              <!-- 發票資訊-個人 -->
+              <div class="col bg-bgc-paper rounded-3 p-6 mb-6">
+                <h5 class="mb-3 fw-medium">發票資訊 <span class="text-danger">*</span></h5>
+                <div class="mb-3">
+                  <div class="form-check form-check-inline me-11">
+                    <input class="form-check-input checked" type="radio" name="personalticket" id="personalticket">
+                    <label class="form-check-label fs-6 text-gray-500" for="personalticket">
+                      個人發票
+                    </label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="bsticket" id="bsticket">
+                    <label class="form-check-label fs-6 text-gray-500" for="bsticket">
+                      公司發票
+                    </label>
+                  </div>
+                </div>
                 <div class="col-6">
-                  <label for="compiled" class="form-label fs-6">
-                    發票統編
+                  <label for="phoneticket" class="form-label fs-6">
+                    手機載具
                     <span class="text-danger">
                       *
                     </span>
                   </label>
-                  <input class="form-control fs-6 border-primary-light" id="compiled" placeholder="例：12345678">
+                  <input class="form-control fs-6 border-primary-light" id="phoneticket" placeholder="例：/AB1CD23">
                 </div>
-                <div class="col-6">
-                  <label for="bsname" class="form-label fs-6">
-                    發票抬頭
-                    <span class="text-danger">
-                      *
-                    </span>
-                  </label>
-                  <input class="form-control fs-6 border-primary-light" id="bsname" placeholder="組織名稱">
+              </div>
+              <!-- 發票資訊-公司 -->
+              <div class="col bg-bgc-paper rounded-3 p-6 mb-6 d-none">
+                <h5 class="mb-3 fw-medium">發票資訊 <span class="text-danger">*</span></h5>
+                <div class="mb-3">
+                  <div class="form-check form-check-inline me-11">
+                    <input class="form-check-input" type="radio" name="personalticket" id="personalticket">
+                    <label class="form-check-label fs-6 text-gray-500" for="personalticket">
+                      個人發票
+                    </label>
+                  </div>
+                  <div class="form-check form-check-inline">
+                    <input class="form-check-input checked" type="radio" name="bsticket" id="bsticket">
+                    <label class="form-check-label fs-6 text-gray-500" for="bsticket">
+                      公司發票
+                    </label>
+                  </div>
+                </div>
+                <div class="row d-flex">
+                  <div class="col-6">
+                    <label for="compiled" class="form-label fs-6">
+                      發票統編
+                      <span class="text-danger">
+                        *
+                      </span>
+                    </label>
+                    <input class="form-control fs-6 border-primary-light" id="compiled" placeholder="例：12345678">
+                  </div>
+                  <div class="col-6">
+                    <label for="bsname" class="form-label fs-6">
+                      發票抬頭
+                      <span class="text-danger">
+                        *
+                      </span>
+                    </label>
+                    <input class="form-control fs-6 border-primary-light" id="bsname" placeholder="組織名稱">
+                  </div>
+                </div>
+              </div>
+              <!-- 備註 -->
+              <div class="col mb-6">
+                <h5>備註<span class="text-gray-500">（選填）</span></h5>
+                <div class="form-floating">
+                  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                  <label for="floatingTextarea">請留言</label>
                 </div>
               </div>
             </div>
-            <!-- 備註 -->
-            <div class="col mb-6">
-              <h5>備註<span class="text-gray-500">（選填）</span></h5>
-              <div class="form-floating">
-                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                <label for="floatingTextarea">請留言</label>
-              </div>
-            </div>
-          </form>
+          </Vform>
           <!-- 同意隱私政策 -->
           <div class="form-check mb-8">
             <input class="form-check-input" type="checkbox" value="" id="agreement" checked>
@@ -318,6 +360,12 @@ export default {
       test: {
         proposalFeedbacks: {},
       },
+      selectedCity: null,
+      cities: [
+        '台北市', '新北市', '桃園市', '台中市', '台南市', '高雄市', '基隆市', '新竹市',
+        '嘉義市', '新竹縣', '苗栗縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '屏東縣',
+        '宜蘭縣', '花蓮縣', '台東縣', '澎湖縣', '金門縣', '連江縣',
+      ],
     };
   },
   computed: {
