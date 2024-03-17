db72bdf5ed5bdaf666c435fbc56f6a7b590b7643
ciel <cielchang0324@gmail.com>
2024-03-17 17:14:04 +0800

    feat: 🧋 修正個人頁面的提案管理頁面
    調整Grid完成RWD不破版。新增篩選提案的功能。

--- src/views/front/UbProposal.vue
+++ src/views/front/UbProposal.vue
@@ -6,9 +6,10 @@
     </div>
     <template v-if="userProposals">
       <div class="row g-0 mt-9">
-        <div class="col-3">
+        <div class="col-5 col-md-3">
           <p class="text-gray-600 mb-1">專案狀態</p>
-          <select class="form-select border border-primary-light text-gray-500" aria-label="Default select">
+          <select class="form-select border border-primary-light text-gray-500" aria-label="Default select"
+            v-model="filterselect">
             <option value="all">全部專案</option>
             <option value="draft">草稿</option>
             <option value="review">審核中</option>
@@ -16,17 +17,22 @@
           </select>
         </div>
       </div>
-      <div class="row mt-13">
+      <div class="row mt-13 mb-20">
         <div class="col-12">
           <div class="l-proposalCardGapY">
-            <ProposalRecordCard v-for="item in userProposals" :key="item.proposalID" :proposal-data="item"
-            @update-proposal-record-data="getUserProposals"
-            ></ProposalRecordCard>
+            <template v-if="filteredProposals.length===0">
+              <p>目前篩選的條件沒有提案</p>
+            </template>
+            <template v-else>
+              <ProposalRecordCard v-for="item in filteredProposals" :key="item.proposalID" :proposal-data="item"
+                @update-proposal-record-data="getUserProposals"></ProposalRecordCard>
+            </template>
           </div>
-          <button class="d-flex m-auto btn btn-primary px-13 mt-20 mb-30 align-items-center">
+          <!-- 沒有實作這個功能 -->
+          <!-- <button class="d-flex m-auto btn btn-primary px-13 mt-20 mb-30 align-items-center">
             <span class="lh-1">載入更多</span>
             <OutlineCubeIcon width="24" height="24" class="ms-2"></OutlineCubeIcon>
-          </button>
+          </button> -->
         </div>
       </div>
     </template>
@@ -41,7 +47,7 @@ import mixinFullScreenLoading from '@/mixins/mixinFullScreenLoading';
 import mixinSwalToast from '@/mixins/mixinSwalToast';
 
 import ProposalRecordCard from '@/components/member/ProposalRecordCard.vue';
-import OutlineCubeIcon from '@/components/icons/OutlineCubeIcon.vue';
+// import OutlineCubeIcon from '@/components/icons/OutlineCubeIcon.vue'; 因為把button移除為了避免eslint錯誤所以先註解
 
 const { VITE_URL } = import.meta.env;
 
@@ -49,6 +55,8 @@ export default {
   data() {
     return {
       userProposals: '',
+      filterselect: 'all',
+      filteredProposals: [],
     };
   },
   methods: {
@@ -59,6 +67,7 @@ export default {
         .then((res) => {
           this.hideFullScreenLoading();
           this.userProposals = Object.values(res.data.data.result);
+          this.filteredProposals = [...this.userProposals];
         })
         .catch(() => {
           this.hideFullScreenLoading();
@@ -66,9 +75,33 @@ export default {
         });
     },
   },
+  computed: {
+    draftProposals() {
+      return this.userProposals.filter((item) => item.proposalStatus === 'draft');
+    },
+    reviewProposals() {
+      return this.userProposals.filter((item) => item.proposalStatus === 'review');
+    },
+    activeProposals() {
+      return this.userProposals.filter((item) => item.proposalStatus === 'active');
+    },
+  },
+  watch: {
+    filterselect(n) {
+      if (n === 'all') {
+        this.filteredProposals = this.userProposals;
+      } else if (n === 'draft') {
+        this.filteredProposals = this.draftProposals;
+      } else if (n === 'review') {
+        this.filteredProposals = this.reviewProposals;
+      } else if (n === 'active') {
+        this.filteredProposals = this.activeProposals;
+      }
+    },
+  },
   components: {
     ProposalRecordCard,
-    OutlineCubeIcon,
+    // OutlineCubeIcon,  因為把button移除為了避免eslint錯誤所以先註解
   },
   mounted() {
     this.getUserProposals();
